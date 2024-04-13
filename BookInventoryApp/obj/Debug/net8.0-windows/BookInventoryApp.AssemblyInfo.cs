b3bfee398afcc0093992baf3493699201fa11d33
Robert <robert.nording@wixcorp.com>
2024-04-13 11:07:25 -0700

    database

--- BookInventoryApp/obj/Debug/net8.0-windows/BookInventoryApp.AssemblyInfo.cs
+++ BookInventoryApp/obj/Debug/net8.0-windows/BookInventoryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookInventoryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eff7c10c847bea55083306243b0955c3d7dcd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookInventoryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookInventoryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
